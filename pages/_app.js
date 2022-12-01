3db811e95e351431f4bde0fa82275bfaa794c844
Rob <robgmerrill@gmail.com>
2022-12-01 12:45:07 -0800

    add navbar to global app

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,14 @@
 import "../styles/globals.css";
+import Navbar from "../components/Navbar";
 
 // Wrapper or layout that surrounds - header or footer components for example or authentication
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Navbar />
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
